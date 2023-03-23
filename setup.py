33ec08186710882aff7b314c95637ff1409338e1
Neil Macintyre <6704857+neilmacintyre@users.noreply.github.com>
2023-03-23 10:52:15 -0400

    Define version in setup file

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import sys
-from xml.etree.ElementTree import ElementTree
+# import sys
+# from xml.etree.ElementTree import ElementTree
 
-try:
-    root = ElementTree(None, 'stack.xml')
-    version = root.findtext('version')
-except Exception, e:
-    print >> sys.stderr, 'Could not extract version from your stack.xml:\n%s' % e
-    sys.exit(-1)
+# try:
+#     root = ElementTree(None, 'stack.xml')
+#     version = root.findtext('version')
+# except Exception, e:
+#     print >> sys.stderr, 'Could not extract version from your stack.xml:\n%s' % e
+#     sys.exit(-1)
 
-sys.path.insert(0, 'src')
+# sys.path.insert(0, 'src')
 
 setup(name = 'genmsg',
-      version = version,
+      version = "0.3.10",
       packages = ['genmsg'],
       package_dir = {'': 'src'},
       scripts = [],
